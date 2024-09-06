72737d35463d2c15956244f619638d1abe4d2d84
olgui <olga.concha@tsoftglobal.com>
2024-09-06 12:25:37 -0400

    se realizan ejercicios correspondientes al producto de 2 números

--- src/Ejercicios/Producto_2_numeros.java
+++ src/Ejercicios/Producto_2_numeros.java
@@ -1,4 +1,13 @@
 package Ejercicios;
 
 public class Producto_2_numeros {
+
+    public static void main(String[] args) {
+        int a, b, prod;
+        a = 7;
+        b = 8;
+        prod = a*b;
+        System.out.println("El resultado del producto de los 2 números es " + prod);
+
+    }
 }
