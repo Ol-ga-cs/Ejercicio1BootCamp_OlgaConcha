2db02006654c86315b28033228175e8c647e2ad1
olgui <olga.concha@tsoftglobal.com>
2024-09-06 15:23:48 -0400

    se realizan ejercicios de paridad y números primos

--- src/Ejercicios/Paridad.java
+++ src/Ejercicios/Paridad.java
@@ -1,4 +1,13 @@
 package Ejercicios;
 
 public class Paridad {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a % 2 == 0){
+            System.out.println("El número es par");
+        }
+        else{
+            System.out.println("El número es impar");
+        }
+    }
 }
--- src/Ejercicios/Primos.java
+++ src/Ejercicios/Primos.java
@@ -1,4 +1,28 @@
 package Ejercicios;
 
 public class Primos {
-}
+
+    public static boolean CalculaPrimo(int a){
+        int cont = 2;
+        boolean primo = true;
+        while ((primo) && (cont!=a))
+        {
+            if (a % cont == 0)
+                primo = false;
+                cont++;
+        }
+        return primo;
+    }
+
+    public static void main(String[] args) {
+        int b = 29;
+        if (CalculaPrimo(b))
+            System.out.println("Es numero primo");
+        else
+            System.out.println("NO es numero primo");
+
+        }
+
+    }
+
+
