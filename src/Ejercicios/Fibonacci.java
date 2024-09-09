d0ab8f4b5af65338135e2de0ad793f1de9beb615
olgui <olga.concha@tsoftglobal.com>
2024-09-09 11:55:53 -0300

    se realizan ejercicios de Fibonacci e Invertir Número

--- src/Ejercicios/Fibonacci.java
+++ src/Ejercicios/Fibonacci.java
@@ -1,4 +1,25 @@
 package Ejercicios;
 
+import com.sun.source.tree.WhileLoopTree;
+
 public class Fibonacci {
+    public static void main(String[] args){
+        int n = 10;
+        int x = 0;
+        int y = 1;
+        int fibo = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            fibo = x + y;
+            x = y;
+            y = fibo;
+
+
+            System.out.println("El Fibonacci de " +n +  " es: " +fibo);
+        }
+    }
+
 }
+
+
--- src/Ejercicios/Inversion_numero.java
+++ src/Ejercicios/Inversion_numero.java
@@ -1,4 +1,17 @@
 package Ejercicios;
 
 public class Inversion_numero {
+    public static void main(String[] args){
+        int a = 54321;
+        int invierte = 0;
+        int original = a;
+     while (a != 0){
+         int ultimo = a % 10;
+         invierte = (invierte * 10 ) + ultimo;
+         a = a / 10;
+     }
+        System.out.println("El número original es: " +original);
+        System.out.println("El número invertido es " +invierte);
+
+    }
 }
