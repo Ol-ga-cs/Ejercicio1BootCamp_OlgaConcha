5fabcee655f08e9a08532ea7607245b08172b680
olgui <olga.concha@tsoftglobal.com>
2024-09-09 18:06:25 -0300

    se realizan los ejercicios de números perfectos

--- src/Ejercicios/Numeros_Perfectos.java
+++ src/Ejercicios/Numeros_Perfectos.java
@@ -1,4 +1,19 @@
 package Ejercicios;
 
 public class Numeros_Perfectos {
+    public static void main(String[] args) {
+        int suma = 0;
+        int a = 28;
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
+                suma = suma + i;
+            }
+        }
+        if (suma == a) {
+            System.out.println("El número " +a+ " es Perfecto");
+        } else {
+            System.out.println("El número " +a+ " no es perfecto");
+
+        }
+    }
 }
