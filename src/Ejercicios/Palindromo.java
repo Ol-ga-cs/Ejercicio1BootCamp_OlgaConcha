80cc0e5d8d0f3cc0e9ee945a6ae4f25ffd7f1524
olgui <olga.concha@tsoftglobal.com>
2024-09-09 13:17:53 -0300

    se realiza el ejercicio de Palindromo

--- src/Ejercicios/Palindromo.java
+++ src/Ejercicios/Palindromo.java
@@ -1,4 +1,19 @@
 package Ejercicios;
 
 public class Palindromo {
+    public static void main(String[] args){
+        int a = 1221;
+        int invierte = 0;
+        int original = a;
+        while (a != 0){
+            int ultimo = a % 10;
+            invierte = (invierte * 10 ) + ultimo;
+            a = a / 10;
+        }
+        if (original == invierte){
+           System.out.println("El número " +original+ " es palíndromo");
+        }
+        else System.out.println("el número " +original+ " no es palíndromo");
+    }
+
 }
