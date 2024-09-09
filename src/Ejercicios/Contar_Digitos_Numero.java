326ad8190e8df637b69ec6d12cb018e605899d9f
olgui <olga.concha@tsoftglobal.com>
2024-09-09 20:22:47 -0300

    se realizan los ejercicios de MCD y conteo de dígitos

--- src/Ejercicios/Contar_Digitos_Numero.java
+++ src/Ejercicios/Contar_Digitos_Numero.java
@@ -1,8 +1,16 @@
 package Ejercicios;
 
 public class Contar_Digitos_Numero {
+    public static void main(String[] args) {
+        int cantidad = 0;
+        int a = 987654;
+        int original = a;
 
+        while (a > 0){
+            a = a / 10;
+            cantidad++;
+        }
 
-
-
+        System.out.println("El número "+original + " tiene " + cantidad + " dígitos");
+    }
 }
--- src/Ejercicios/MCD.java
+++ src/Ejercicios/MCD.java
@@ -1,4 +1,16 @@
 package Ejercicios;
 
 public class MCD {
-}
+    public static void main(String[] args){
+        int a = 48;
+        int b = 18;
+        int temp = b;
+        int aoriginal = a;
+        int boriginal = b;
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("El máximo común divisor entre " +aoriginal+ " y " +boriginal+ " es: "+a);
+    }}
