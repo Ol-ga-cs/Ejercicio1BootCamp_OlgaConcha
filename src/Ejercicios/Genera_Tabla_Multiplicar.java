f0f4ce785de1202b51aae1fd2adcddc64f61004e
olgui <olga.concha@tsoftglobal.com>
2024-09-10 11:22:18 -0300

    se realizaa la tabla de multiplicar

--- src/Ejercicios/Genera_Tabla_Multiplicar.java
+++ src/Ejercicios/Genera_Tabla_Multiplicar.java
@@ -1,4 +1,15 @@
 package Ejercicios;
 
 public class Genera_Tabla_Multiplicar {
+
+    public static void main(String[] args) {
+        int a = 7;
+        int limite = 12;
+        
+        System.out.println("Tabla de multiplicar del número " + a + ":");
+
+        for (int i = 1; i <= limite; i++) {
+            System.out.println(a + " x " + i + " = " + (a * i));
+        }
+    }
 }
