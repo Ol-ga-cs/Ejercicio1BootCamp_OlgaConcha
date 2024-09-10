b259ea4aaae6f0c7874cc19d4e1ba9ccdc57b8d8
olgui <olga.concha@tsoftglobal.com>
2024-09-10 16:18:08 -0300

    se realizan ejercicios de raiz cuadrada y cuadrado perfecto

--- src/Ejercicios/E29_Numero_Cuadrado_Perfecto.java
+++ src/Ejercicios/E29_Numero_Cuadrado_Perfecto.java
@@ -2,26 +2,16 @@ package Ejercicios;
 
 public class E29_Numero_Cuadrado_Perfecto {
     public static void main(String[] args) {
-        int a = 64;
-       /* if (esCuadradoPerfecto(a)) {
-            System.out.println(a + " es un cuadrado perfecto");
-        } else {
-            System.out.println(a + " no es un cuadrado perfecto");
+        int numero = 64;
+        int raiz = 0;
+        while (raiz * raiz <= numero) {
+            if (raiz * raiz == numero) {
+                System.out.println(numero + " es un cuadrado perfecto.");
+                return;
+            }
+            raiz++;
         }
-    }*/
-
-    //public static boolean esCuadradoPerfecto(int a) {
-        for (int i = 1; i <= (a / 2); i++) {
-            if (i * i == a) {
-                System.out.println(a + " es un cuadrado perfecto");//return true;
-
-            }
-            else if (i * i != a) {
-                System.out.println(a + " no es un cuadrado perfecto");
-            }
-
-
-     //   return false;
+        System.out.println(numero + " no es un cuadrado perfecto.");
     }
-}
+
 }
--- src/Ejercicios/Raiz_Cuadrada.java
+++ src/Ejercicios/Raiz_Cuadrada.java
@@ -1,4 +1,23 @@
 package Ejercicios;
 
 public class Raiz_Cuadrada {
+
+         public static void main(String[] args) {
+            double a = 64;
+            double original = a;
+            if (a < 0) {
+                System.out.println("El número debe ser no negativo.");
+            }
+
+            while ((original * original - a) > 0 || (a - original * original) > 0) {
+                original = (original + a / original) / 2;
+            }
+
+            System.out.println("La raíz cuadrada de " + a + " es: " + original);
+        }
 }
+
+
+
+
+
